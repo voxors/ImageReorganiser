3c2f163b316b7bb6edce6f90b7ed460ebcbfe584
voxors <jonathan.croteau.dicaire@gmail.com>
2016-12-04 15:35:58 -0500

    Set maximun height of main windows

--- ImageReorganiser/mainwindow.cpp
+++ ImageReorganiser/mainwindow.cpp
@@ -9,6 +9,8 @@ MainWindow::MainWindow(QWidget *parent) :
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+  this->setMaximumHeight(this->height());
+  this->setMinimumHeight(this->height());
   imageLogicThread = nullptr;
   LoadParams();
 }
